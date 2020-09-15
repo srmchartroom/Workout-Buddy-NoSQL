6aa6597d89c66e293f8ca68183240e06e6f22c97
srmchartroom <sean@chartroomcreative.com>
2020-09-15 18:05:25 -0400

    updated process.env.mongodb_uri

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 let db = require("../models/index.js");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
